5d8afb6d94931b214c07b964ce3ef52bce847c23
Anders <hello@andersio.co>
2016-02-25 10:49:42 +0800

    Initial implementation of TableViewAdapter for NSTableView, supporting view-based cells from storyboards and nibs.

---
+++ MantleData/ReactiveBindings-OSX.swift
@@ -0,0 +1,33 @@
+//
+//  ReactiveBindings.swift
+//  MantleData
+//
+//  Created by Anders on 14/12/2015.
+//  Copyright © 2015 Anders. All rights reserved.
+//
+
+import ReactiveCocoa
+
+extension NSObject {
+	public func bindKeyPath<Producer: SignalProducerType where Producer.Value: AnyObject>(keyPath: String, from producer: Producer) {
+		producer
+			.startWithNext { [weak self] value in
+				self?.setValue(value, forKeyPath: keyPath)
+			}
+	}
+
+	public func bindKeyPath<Producer: SignalProducerType where Producer.Value: CocoaBridgeable>(keyPath: String, from producer: Producer) {
+		producer
+			.startWithNext { [weak self] value in
+				self?.setValue(value.cocoaValue, forKeyPath: keyPath)
+		}
+	}
+
+	public func bindKeyPath<Producer: SignalProducerType where Producer.Value: AnyObject>(keyPath: String, onMainQueueFrom producer: Producer) {
+		bindKeyPath(keyPath, from: producer.observeOn(UIScheduler()))
+	}
+
+	public func bindValue<Producer: SignalProducerType where Producer.Value: CocoaBridgeable>(keyPath: String, onMainQueueFrom producer: Producer) {
+		bindKeyPath(keyPath, from: producer.observeOn(UIScheduler()))
+	}
+}
