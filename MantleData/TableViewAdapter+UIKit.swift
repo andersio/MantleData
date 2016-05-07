a8d07acd85513513a60bdddbb5b5e434fc8e1864
Anders <hello@andersio.co>
2016-05-08 02:17:28 +0800

    Refined the interface of `TableViewAdapter`. Adapted `TableViewAdapter` to the revamped `ViewModelSet`.

--- MantleData/TableViewAdapter+UIKit.swift
+++ MantleData/TableViewAdapter+UIKit.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2015 Ik ben anders. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import ReactiveCocoa
 
@@ -17,14 +16,21 @@ public enum TableViewAdapterRegistration {
 
 final public class TableViewAdapter<V: ViewModel>: NSObject, UITableViewDataSource {
 	private let set: ViewModelSet<V>
-	private var isEmpty: Bool = true
 
 	private var cellConfigurators: [(reuseIdentifier: String, configurator: (cell: UITableViewCell, viewModel: V) -> Void)?]
-	public var isUniform = false
-	public var shouldReloadRowsForUpdatedObjects = false
-	public var rowAnimation: UITableViewRowAnimation = .Automatic
-	public var sectionNameTransform: ((Int, String?) -> String?)?
-	public var emptySetHandler: (Bool -> Void)?
+	private var isUniform = false
+
+	private var shouldReloadRowsForUpdatedObjects = false
+
+	private var insertingAnimation: UITableViewRowAnimation = .Automatic
+	private var deletingAnimation: UITableViewRowAnimation = .Automatic
+	private var updatingAnimation: UITableViewRowAnimation = .Automatic
+
+	private var sectionNameMapper: ((position: Int, persistedName: String?) -> String?)?
+
+	private var isEmpty: Bool = true
+	public var emptiedObserver: (() -> Void)?
+	public var unemptiedObserver: (() -> Void)?
 
 	public init(set: ViewModelSet<V>) {
 		self.set = set
@@ -54,6 +60,33 @@ final public class TableViewAdapter<V: ViewModel>: NSObject, UITableViewDataSour
 		return self
 	}
 
+	public func setAnimation(inserting insertingAnimation: UITableViewRowAnimation? = nil,
+	                         deleting deletingAnimation: UITableViewRowAnimation? = nil,
+													 updating updatingAnimation: UITableViewRowAnimation? = nil)
+													 -> TableViewAdapter {
+		self.insertingAnimation = insertingAnimation ?? self.insertingAnimation
+		self.deletingAnimation = deletingAnimation ?? self.deletingAnimation
+		self.updatingAnimation = updatingAnimation ?? self.updatingAnimation
+
+		return self
+	}
+
+	public func mapSectionName(using transform: (position: Int, persistedName: String?) -> String?) -> TableViewAdapter {
+		sectionNameMapper = transform
+		return self
+	}
+
+	public func reloadUpdatedRows(enabling flag: Bool = true) -> TableViewAdapter {
+		shouldReloadRowsForUpdatedObjects = flag
+		return self
+	}
+
+	public func on(emptied emptied: (() -> Void)?, unemptied: (() -> Void)?) -> TableViewAdapter {
+		emptiedObserver = emptied ?? emptiedObserver
+		unemptiedObserver = unemptied ?? unemptiedObserver
+		return self
+	}
+
 	public func bind(tableView: UITableView) -> Disposable {
 		tableView.dataSource = self
 
@@ -70,27 +103,27 @@ final public class TableViewAdapter<V: ViewModel>: NSObject, UITableViewDataSour
 
 					if let indexSet = changes.deletedSections {
 						tableView.deleteSections(NSMutableIndexSet(converting: indexSet),
-							withRowAnimation: self.rowAnimation)
+							withRowAnimation: self.deletingAnimation)
 					}
 
 					if let indexSet = changes.reloadedSections {
 						tableView.reloadSections(NSMutableIndexSet(converting: indexSet),
-							withRowAnimation: self.rowAnimation)
+							withRowAnimation: self.updatingAnimation)
 					}
 
 					if let indexPaths = changes.deletedRows {
 						tableView.deleteRowsAtIndexPaths(indexPaths.map { NSIndexPath(converting: $0) },
-							withRowAnimation: self.rowAnimation)
+							withRowAnimation: self.deletingAnimation)
 					}
 
 					if self.shouldReloadRowsForUpdatedObjects, let indexPaths = changes.updatedRows {
 						tableView.reloadRowsAtIndexPaths(indexPaths.map { NSIndexPath(converting: $0) },
-							withRowAnimation: self.rowAnimation)
+							withRowAnimation: self.updatingAnimation)
 					}
 
 					if let indexSet = changes.insertedSections {
 						tableView.insertSections(NSMutableIndexSet(converting: indexSet),
-							withRowAnimation: self.rowAnimation)
+							withRowAnimation: self.insertingAnimation)
 					}
 
 					if let indexPathPairs = changes.movedRows {
@@ -102,41 +135,43 @@ final public class TableViewAdapter<V: ViewModel>: NSObject, UITableViewDataSour
 
 					if let indexPaths = changes.insertedRows {
 						tableView.insertRowsAtIndexPaths(indexPaths.map { NSIndexPath(converting: $0) },
-							withRowAnimation: self.rowAnimation)
+							withRowAnimation: self.insertingAnimation)
 					}
 
 					tableView.endUpdates()
 				}
 
-				if !self.isEmpty && self.set.numberOfObjects == 0 {
+				if !self.isEmpty && self.set.map({ $0.count }).reduce(0, combine: +) == 0 {
 					self.isEmpty = true
-					self.emptySetHandler?(true)
+					self.emptiedObserver?()
 				} else if self.isEmpty {
 					self.isEmpty = false
-					self.emptySetHandler?(false)
+					self.unemptiedObserver?()
 				}
 		}
 	}
 
+	/// MARK: `UITableViewDataSource` conformance
+
 	public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-		return set.numberOfSections
+		return set.count
 	}
 
 	public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-		let sectionName = set.sectionName(for: section)
-		return sectionNameTransform?(section, sectionName) ?? sectionName
+		let sectionName = set[section.typeErased()].name.value
+		return sectionNameMapper?(position: section, persistedName: sectionName) ?? sectionName
 	}
 
 	public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let (reuseIdentifier, configurator) = cellConfigurators[isUniform ? 0 : indexPath.section]!
 		let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier,
 		                                                       forIndexPath: indexPath)
-		configurator(cell: cell, viewModel: set[indexPath])
+		configurator(cell: cell, viewModel: set[indexPath.section.typeErased()][indexPath.row.typeErased()])
 
 		return cell
 	}
 
 	public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return set.numberOfRows(for: section)
+		return set[section.typeErased()].count
 	}
 }
